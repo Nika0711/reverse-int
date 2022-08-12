1e112ecb8913eaf8f6aa3383770798e7beb9355f
Veronika Pakeleva <veronika.pakeleva@gmail.com>
2022-08-12 23:45:02 +0300

    new code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    const negative = (num<0) ? true : false;
+    const reversedNum=Number(Math.abs(num).toString().split("").reverse().join(""));
+
+    return negative ? reversedNum : reversedNum;
 }
+
+
